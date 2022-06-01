5fc663e60c01bc596e84ac20c5f57eed446759eb
CooperGidge <98977687+CooperGidge@users.noreply.github.com>
2022-06-01 13:54:09 -0400

    Initial commit

---
+++ src/List.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author cogid4775
+ */
+public class List {
+    
+    private ArrayList<String> list;
+    
+    public List() {
+        list = new ArrayList<String>();
+    }
+    
+    public void addItem(String s) {
+        list.add(s);
+    }
+    
+    public String getItem(int n) {
+        return list.get(n);
+    }
+    
+    public int getSize() {
+        return list.size();
+    }
+    
+}
